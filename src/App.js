1c4345882e6ac0e24c728d7cd5e30a4efac97706
Andrés  Pillajo <andrespillajo@MacBook-Air-de-Andres.local>
2024-09-05 17:36:58 -0500

    funcionalidad completa corregido error localhost:3001

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { DatosContexto } from './components/DatosContexto';
-import { useClientes } from './hooks/useClientes';
 import { AppRoutes } from './routes/AppRoutes';
 import '../src/styles/principalApp.css'
 
 function App() {
-  const clientes = useClientes();
+  
   return (
     <div className="App">
-      <DatosContexto.Provider value={{clientes}}>
         <AppRoutes />
-      </DatosContexto.Provider>
     </div>
   );
 }
